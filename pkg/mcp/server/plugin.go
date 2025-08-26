5094e304dde401488451a852f17a2d342ae34194
wangkai19 <wangkai19@corp.netease.com>
2025-08-26 18:17:21 +0800

    allow only rest server

--- pkg/mcp/server/plugin.go
+++ pkg/mcp/server/plugin.go
@@ -245,7 +245,8 @@ func parseConfigCore(configJson gjson.Result, config *McpServerConfig, opts *Con
 			}
 		}
 	} else {
-		return errors.New("either 'server' or 'toolSet' field must be present in the configuration")
+		// qz logic to escape global config
+		return nil
 	}
 
 	// Parse allowTools - this might need adjustment for composed servers
@@ -391,9 +392,7 @@ func Load(options ...CtxOption) {
 }
 
 func Initialize() {
-	if globalContext.servers == nil {
-		panic("At least one mcpserver needs to be added.")
-	}
+	// qz logic to allow only rest server
 	wrapper.SetCtx(
 		"mcp-server",
 		wrapper.PrePluginStartOrReload[McpServerConfig](onPluginStartOrReload),
